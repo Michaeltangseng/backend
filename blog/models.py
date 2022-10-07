76f9e8fa102f238226046fa3114216b80fc75922
“limu” <3463904031@qq.com>
2022-10-07 13:01:29 +0800

    这是backend的代码

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.CharField(max_length = 500)
+    class Meta:
+        db_table = 't_blog'
